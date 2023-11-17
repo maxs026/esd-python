7a47e011849f473ee8e9d9db9cf667730279fa00
maxs026 <150455778+maxs026@users.noreply.github.com>
2023-11-17 13:17:14 +0100

    loops.py

--- loops.py
+++ loops.py
@@ -1,6 +1,7 @@
-EXERCICES PYTHON :
-
 def multiplie(a, b):
+
+    Multiplie deux nombres en utilisant des additions successives.
+    
     resultat = 0
     while b > 0:
         resultat += a
@@ -8,13 +9,19 @@ def multiplie(a, b):
     return resultat
 
 def puissance(base, exposant):
-  
+    
+    Calcule la puissance d'un nombre par multiplication successive.
+    
     resultat = 1
     while exposant > 0:
         resultat = multiplie(resultat, base)
         exposant -= 1
     return resultat
 
-Exemples d'utilisation : 
+Exemples d'utilisation :
+
 resultat_multiplication = multiplie(5, 3)
 resultat_puissance = puissance(2, 4)
+
+print(f"Résultat de la multiplication : {resultat_multiplication}")
+print(f"Résultat de la puissance : {resultat_puissance}")
